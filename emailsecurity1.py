c13d28176bc983e28bc9b5b73ee539f9270bd73c
ashnajay <ashna.j.kumar.24@dartmouth.edu>
2022-03-01 18:51:50 -0500

    Censored server credentials

--- emailsecurity1.py
+++ emailsecurity1.py
@@ -5,8 +5,8 @@ import MySQLdb as db
 
 from email.mime.text import MIMEText
 
-# Connect to database
-mycon=db.connect(host="localhost",user="ashna",passwd="ashna21",db="HomeAutoMation")
+# Connect to database (credentials censored)
+mycon=db.connect(host="localhost",user="*****",passwd="*******",db="HomeAutoMation")
 cursor=mycon.cursor()
 
 # Execute query (below line is actual query used but to ensure there is at least one result, the uncommented query uses a fixed time)
